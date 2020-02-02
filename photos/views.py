0cfebc2e3ba1d6d2c90d3177c546898e7af8e8a8
skimanikings <kimanisimon856@gmail.com>
2020-02-02 21:27:39 +0300

    define the search results function that returns the search

--- photos/views.py
+++ photos/views.py
@@ -4,3 +4,17 @@ from django.shortcuts import render
 
 def home(request):
     return render (request, 'home.html')
+
+
+def search_results(request):
+    
+    if 'article' in request.GET and request.GET["article"]:
+        search_term = request.GET.get("article")
+        searched_articles = Article.search_by_title(search_term)
+        message = f"{search_term}"
+
+        return render(request, 'all-news/search.html',{"message":message,"articles": searched_articles})
+
+    else:
+        message = "You haven't searched for any term"
+        return render(request, 'all-news/search.html',{"message":message})
