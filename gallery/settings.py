7dd038cd21afb51f2ffda3d03c53cf5920a135df
skimanikings <kimanisimon856@gmail.com>
2020-02-02 21:50:34 +0300

    import the settings and static in the news url

--- gallery/settings.py
+++ gallery/settings.py
@@ -63,11 +63,11 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
             ],
         },
     },
 ]
-
 WSGI_APPLICATION = 'gallery.wsgi.application'
 
 
--- photos/urls.py
+++ photos/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import url 
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
       url('',views.home,name = 'home'),
