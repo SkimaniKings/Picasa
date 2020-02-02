8cda632bd0d9aa732cfc21724588d0f418176b4b
skimanikings <kimanisimon856@gmail.com>
2020-02-02 21:51:24 +0300

    add the settings to the app url

--- photos/urls.py
+++ photos/urls.py
@@ -8,3 +8,6 @@ urlpatterns = [
       url(r'^search/', views.search_results, name='search_results')
       
 ]
+
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
