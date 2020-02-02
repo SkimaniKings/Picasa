e28defb6cd2f85f387986f6cb9c4282c7c1e7bb8
skimanikings <kimanisimon856@gmail.com>
2020-02-02 20:14:43 +0300

    created the picture model

--- photos/models.py
+++ photos/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+import datetime as dt
 
 # Create your models here.
+class Picture(models.Model):
+    title = models.CharField(max_length =30)
+    image = models.ImageField(upload_to = 'images/', default="")
+    posted_date= models.DateTimeField(auto_now_add=True)
+    description = models.CharField(max_length =200)
+    location = models.ForeignKey('Location', on_delete=models.CASCADE)
+    category = models.ForeignKey('Category', on_delete=models.CASCADE)
