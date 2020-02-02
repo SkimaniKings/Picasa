921c6bd220b2edb953c63c08b849e05338bca956
skimanikings <kimanisimon856@gmail.com>
2020-02-02 21:40:11 +0300

    define search results function

--- photos/views.py
+++ photos/views.py
@@ -10,10 +10,18 @@ def search_results(request):
     
     if 'locate' in request.GET and request.GET["locate"]:
         search_term = request.GET.get("locate")
-        searched_images = Article.search_by_title(search_term)
+        searched_images = Picture.search_by_location(search_term)
         message = f"{search_term}"
 
         return render(request, 'search.html',{"message":message,"locate": searched_images})
+    elif 'locate' in request.GET and request.GET["locate"]:
+        search_term = request.GET.get("locate")
+        searched_images = Picture.search_by_category(search_term)
+        message = f"{search_term}"
+
+        return render(request, 'search.html',{"message":message,"locate": searched_images})
+        
+        
 
     else:
         message = "You haven't searched for any term"
