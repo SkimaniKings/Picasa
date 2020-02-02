0ff89819b6705279d990c923b39900b33b6f156f
skimanikings <kimanisimon856@gmail.com>
2020-02-02 20:17:23 +0300

    created the category model

--- photos/models.py
+++ photos/models.py
@@ -7,5 +7,6 @@ class Picture(models.Model):
     image = models.ImageField(upload_to = 'images/', default="")
     posted_date= models.DateTimeField(auto_now_add=True)
     description = models.CharField(max_length =200)
-    location = models.ForeignKey('Location', on_delete=models.CASCADE)
-    category = models.ForeignKey('Category', on_delete=models.CASCADE)
+   
+class Category(models.Model):
+    category = models.CharField(max_length =30)
