a20f073faa70d90fbc61a6543b6df3018abc9158
skimanikings <kimanisimon856@gmail.com>
2020-02-02 20:29:31 +0300

    add the def str that returns the querries

--- photos/models.py
+++ photos/models.py
@@ -9,10 +9,19 @@ class Picture(models.Model):
     description = models.CharField(max_length =200)
     location = models.ForeignKey('Location')
     category = models.ForeignKey('Category')
+    
+     def __str__(self):
+        return self.description
 
    
 class Category(models.Model):
     category = models.CharField(max_length =30)
+    
+     def __str__(self):
+        return self.category
 
 class Location(models.Model):
     location = models.CharField(max_length =30)
+    
+    def __str__(self):
+        return self.location
