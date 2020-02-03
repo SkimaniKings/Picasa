1e364fa8ba80accbf32bf21f3b253cdd4872982a
skimanikings <kimanisimon856@gmail.com>
2020-02-03 10:29:02 +0300

    querry the the images to be displayed on the home page

--- photos/views.py
+++ photos/views.py
@@ -1,26 +1,10 @@
 from django.shortcuts import render
+from .models import Picture
 
 # Create your views here.
 
 def home(request):
-    return render (request, 'home.html')
+    images = Picture.objects.all()
+    return render (request, 'home.html', {"images":images})
 
 
-def search_results(request):
-    
-    if 'locate' in request.GET and request.GET["locate"]:
-        search_term = request.GET.get("locate")
-        searched_images = Picture.search_by_location(search_term)
-        message = f"{search_term}"
-
-        return render(request, 'search.html',{"message":message,"locate": searched_images})
-    elif 'locate' in request.GET and request.GET["locate"]:
-        search_term = request.GET.get("locate")
-        searched_images = Picture.search_by_category(search_term)
-        message = f"{search_term}"
-
-        return render(request, 'search.html',{"message":message,"locate": searched_images})
-        
-    else:
-        message = "You haven't searched for any term"
-        return render(request, 'search.html',{"message":message})
