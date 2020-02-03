29a45621e511d3f9cb6ffb5c95b1e1ade3327fc4
skimanikings <kimanisimon856@gmail.com>
2020-02-03 11:08:12 +0300

    querry the database images to the home page

--- photos/urls.py
+++ photos/urls.py
@@ -4,10 +4,8 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-      url('',views.home,name = 'home'),
-     
+      url('',views.home,name = 'home'),    
       
 ]
-
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
