27882ecd8aa45b7319bda07ae77c37d91ea8f74f
skimanikings <kimanisimon856@gmail.com>
2020-02-03 10:18:29 +0300

    add on delete cascade method to the picture modulee

--- photos/models.py
+++ photos/models.py
@@ -7,8 +7,8 @@ class Picture(models.Model):
     image = models.ImageField(upload_to = 'images/', default="")
     posted_date= models.DateTimeField(auto_now_add=True)
     description = models.CharField(max_length =200)
-    location = models.ForeignKey('Location')
-    category = models.ForeignKey('Category')
+    location = models.ForeignKey('Location',on_delete=models.CASCADE)
+    category = models.ForeignKey('Category',on_delete=models.CASCADE)
     
     def __str__(self):
         return self.description
